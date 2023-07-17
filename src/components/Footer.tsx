6b5ae2e5c04c10a773c6b2e38b6c6f5d538b5461
khiem <khiemcan@gmail.com>
2023-07-17 20:49:32 +0700

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <div className="footer">
+            <p>Site uses <a href="https://www.opendota.com/">Opendota's API</a></p>
+            <p>Made by khiemcan@gmail.com</p>
+        </div>
+    )
+}
+
+export default Footer
